b23dff0bf277fe8b4c7beddf87fcf42d704ca19c
Roman Pytalev <rpytalev@gmail.com>
2022-05-21 22:14:34 +0300

    implemented 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,15 @@
+const fs = require('fs');
+const path = require('path');
 
+const fsPromises = fs.promises;
+
+const src = path.join(__dirname, 'files');
+const dest = path.join(__dirname, 'files-copy');
+
+fsPromises.mkdir(dest, { recursive: true }).then(async () => {
+    fs.readdir(src, (_, items) => {
+        items.forEach(async item => {
+            await fsPromises.copyFile(path.join(src, item), path.join(dest,item));
+        });
+    });
+});
