be5ea7efd8c91783835ad152ecc2394afcb2ec00
Roman Pytalev <rpytalev@gmail.com>
2022-05-22 20:03:14 +0300

    fixed 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,7 +6,7 @@ const fsPromises = fs.promises;
 const src = path.join(__dirname, 'files');
 const dest = path.join(__dirname, 'files-copy');
 
-fs.rmdir(dest, {recursive: true}, () => {
+fs.rm(dest, {recursive: true}, () => {
     fsPromises.mkdir(dest, { recursive: true }).then(async () => {
         fs.readdir(src, (_, items) => {
             items.forEach(async item => {
